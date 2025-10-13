4033e0796547effcfa324dc42e8afef628726595
Mokshak Ketan Shah <mokshak18@gmail.com>
2025-10-13 20:30:40 +0530

    Update Header.jsx

--- src/components/Navbar/Header/Header.jsx
+++ src/components/Navbar/Header/Header.jsx
@@ -17,7 +17,8 @@
 
 import React from 'react'
 import './Header.css'
-import header_img from './header_img.png' 
+import header_img from '../../../assets/header_img.png';
+
 
 const Header = () => {
   return (
